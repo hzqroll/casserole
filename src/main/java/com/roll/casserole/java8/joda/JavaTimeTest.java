f80f79feb5e90b362772f13eda32107b4c5fbb1d
hzqroll <haozongqiang@foxmail.com>
2019-08-20 09:16:35 +0800

    joda time

--- src/main/java/com/roll/casserole/java8/joda/JavaTimeTest.java
+++ src/main/java/com/roll/casserole/java8/joda/JavaTimeTest.java
@@ -1,9 +1,10 @@
 package com.roll.casserole.java8.joda;
 
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.MonthDay;
+import net.sf.cglib.core.Local;
+
+import java.time.*;
+import java.util.Set;
 
 /**
  * @author roll
@@ -35,5 +36,34 @@ public class JavaTimeTest {
         LocalTime time = localTime.plusHours(3).plusMinutes(2);
         //00:11:10.584
         System.out.println(time);
+
+        Set<String> set = ZoneId.getAvailableZoneIds();
+        set.forEach(System.out::println);
+
+        ZoneId zoneId = ZoneId.of("Asia/Shanghai");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);//2019-08-20T08:30:31.566
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, zoneId);
+        System.out.println(zonedDateTime);//2019-08-20T08:30:31.566+08:00[Asia/Shanghai]
+
+
+        YearMonth yearMonth = YearMonth.now();
+        System.out.println(yearMonth);
+        System.out.println(yearMonth.lengthOfMonth());
+        System.out.println(yearMonth.isLeapYear());
+
+        YearMonth yearMonth1 = YearMonth.of(2019, 4);
+        System.out.println(yearMonth1);
+        System.out.println(yearMonth1.lengthOfYear());
+        System.out.println(yearMonth1.isLeapYear());
+
+        LocalDate localDate3 = LocalDate.now();
+        LocalDate localDate4 = LocalDate.of(2019, 11, 25);
+        Period period = Period.between(localDate3, localDate4);
+        System.out.println(period.getMonths());
+        System.out.println(period.getDays());
+
+        System.out.println(Instant.now());
     }
 }
