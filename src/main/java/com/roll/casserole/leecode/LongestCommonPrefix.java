31b8d913d2ff0472008c21f9578cfdcc58927e54
hzqroll <haozongqiang@foxmail.com>
2020-04-15 22:37:40 +0800

    tree

---
+++ src/main/java/com/roll/casserole/leecode/LongestCommonPrefix.java
@@ -0,0 +1,41 @@
+package com.roll.casserole.leecode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * <p>
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ * <p>
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 说明:
+ * <p>
+ * 所有输入只包含小写字母 a-z 。
+ * <p>
+ *
+ * @author zongqiang.hao
+ * created on 2020/4/15 10:07 下午.
+ */
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+
+        List<Character> characterList = new ArrayList<>();
+        for (String str : strs) {
+
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
