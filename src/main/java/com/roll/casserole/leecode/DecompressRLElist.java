cf7b3ef08c314b9812b751b10fea10244d6e7b70
hzqroll <haozongqiang@foxmail.com>
2020-04-22 18:51:58 +0800

    leecode解题

---
+++ src/main/java/com/roll/casserole/leecode/DecompressRLElist.java
@@ -0,0 +1,66 @@
+package com.roll.casserole.leecode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给你一个以行程长度编码压缩的整数列表 nums 。
+ * <p>
+ * 考虑每对相邻的两个元素 freq, val] = [nums[2*i], nums[2*i+1]] （其中 i >= 0 ），每一对都表示解压后子列表中有 freq 个值为 val 的元素，你需要从左到右连接所有子列表以生成解压后的列表。
+ * <p>
+ * 请你返回解压后的列表。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：nums = [1,2,3,4]
+ * 输出：[2,4,4,4]
+ * 解释：第一对 [1,2] 代表着 2 的出现频次为 1，所以生成数组 [2]。
+ * 第二对 [3,4] 代表着 4 的出现频次为 3，所以生成数组 [4,4,4]。
+ * 最后将它们串联到一起 [2] + [4,4,4] = [2,4,4,4]。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,1,2,3]
+ * 输出：[1,3,3]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 2 <= nums.length <= 100
+ * nums.length % 2 == 0
+ * 1 <= nums[i] <= 100
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/decompress-run-length-encoded-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author roll
+ * created on 2020/4/22 5:24 下午
+ */
+public class DecompressRLElist {
+    public static int[] decompressRLElist(int[] nums) {
+        List<Integer> resultList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int number = nums[i];
+            i = i + 1;
+            int date = nums[i];
+            if (number > 0) {
+                for (int j = 0; j < number; j++) {
+                    resultList.add(date);
+                }
+            }
+        }
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            result[i] = resultList.get(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{};
+        System.out.println(Arrays.toString(decompressRLElist(a)));
+    }
+}
---
+++ src/main/java/com/roll/casserole/leecode/MinFallingPathSum.java
@@ -0,0 +1,71 @@
+package com.roll.casserole.leecode;
+
+/**
+ * 给定一个方形整数数组 A，我们想要得到通过 A 的下降路径的最小和。
+ * <p>
+ * 下降路径可以从第一行中的任何元素开始，并从每一行中选择一个元素。在下一行选择的元素和当前行所选元素最多相隔一列。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：[[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：12
+ * 解释：
+ * 可能的下降路径有：
+ * [1,4,7], [1,4,8], [1,5,7], [1,5,8], [1,5,9]
+ * [2,4,7], [2,4,8], [2,5,7], [2,5,8], [2,5,9], [2,6,8], [2,6,9]
+ * [3,5,7], [3,5,8], [3,5,9], [3,6,8], [3,6,9]
+ * 和最小的下降路径是 [1,4,7]，所以答案是 12。
+ * <p>
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= A.length == A[0].length <= 100
+ * -100 <= A[i][j] <= 100
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-falling-path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author roll
+ * created on 2020/4/22 5:37 下午
+ */
+public class MinFallingPathSum {
+    public static int minFallingPathSum(int[][] A) {
+        int[] basic = A[0];
+        int result = 10000;
+
+        // 其他位数增加的偏移位
+        for (int i1 = 0; i1 < basic.length; i1++) {
+            int[] next = new int[3];
+            if (i1 == 0) {
+                next = new int[]{0, 1};
+            } else {
+                next[0] = i1 - 1;
+                next[1] = i1;
+                next[2] = i1 + 1;
+            }
+            for (int i2 = 0; i2 < next.length; i2++) {
+                int result1 = 0;
+                for (int i = 0; i < A.length; i++) {
+                    if (A[i].length > next[i2]) {
+                        result1 = result1 + A[i][next[i2]];
+                    } else {
+                        result1 = 1000;
+                    }
+                }
+                if (result > result1) {
+                    result = result1;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] A = new int[][]{{17, 82}, {1, -44}};
+        System.out.println(minFallingPathSum(A));
+    }
+}
