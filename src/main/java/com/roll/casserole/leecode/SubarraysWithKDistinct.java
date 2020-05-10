b55a28b7459a414d48c5ee8a8f52b75053a167e3
hzqroll <haozongqiang@foxmail.com>
2020-05-10 16:20:03 +0800

    官方解题

--- src/main/java/com/roll/casserole/leecode/SubarraysWithKDistinct.java
+++ src/main/java/com/roll/casserole/leecode/SubarraysWithKDistinct.java
@@ -9,37 +9,55 @@ import java.util.Map;
  */
 public class SubarraysWithKDistinct {
     public static int subarraysWithKDistinct(int[] A, int K) {
-        int result = 0;
-        for (int i = 0; i < A.length; i++) {
-            Map<Integer, Integer> iMap = new HashMap<>();
-            iMap.put(A[i], i);
-            if (i == A.length - 1) {
-                if (iMap.size() == K) {
-                    result = result + 1;
-                }
-            } else {
-                for (int j = i + 1; j < A.length; j++) {
-                    if (iMap.size() == K) {
-                        result = result + 1;
-                    }
-                    iMap.put(A[j], j);
-                    if (j == A.length - 1) {
-                        if (iMap.size() == K) {
-                            result = result + 1;
-                        }
-                    }
-                    if (iMap.size() > K) {
-                        break;
-                    }
-                }
-            }
+        Window window1 = new Window();
+        Window window2 = new Window();
+        int ans = 0, left1 = 0, left2 = 0;
+
+        for (int right = 0; right < A.length; ++right) {
+            int x = A[right];
+            window1.add(x);
+            window2.add(x);
+
+            while (window1.different() > K)
+                window1.remove(A[left1++]);
+            while (window2.different() >= K)
+                window2.remove(A[left2++]);
+
+            ans += left2 - left1;
         }
-        return result;
+
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2};
-        int k = 1;
+        int[] nums = new int[]{1, 2,1,2,3};
+        int k = 2;
         System.out.println(subarraysWithKDistinct(nums, k));
+    }
+}
+
+class Window {
+    Map<Integer, Integer> count;
+    int nonzero;
+
+    Window() {
+        count = new HashMap();
+        nonzero = 0;
+    }
+
+    void add(int x) {
+        count.put(x, count.getOrDefault(x, 0) + 1);
+        if (count.get(x) == 1)
+            nonzero++;
+    }
+
+    void remove(int x) {
+        count.put(x, count.get(x) - 1);
+        if (count.get(x) == 0)
+            nonzero--;
+    }
+
+    int different() {
+        return nonzero;
     }
 }
